b2bbd11926bde5ea12f56f82d3592fc9180d8c81
Tim Kutcher <tkutcher@outlook.com>
2019-01-09 15:36:15 -0500

    parsing args correctly, no functionality for what they do though

--- src/main/java/com/github/tkutcher/jgrade/Main.java
+++ src/main/java/com/github/tkutcher/jgrade/Main.java
@@ -1,5 +1,13 @@
 package com.github.tkutcher.jgrade;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -35,17 +43,58 @@ public final class Main {
 
 
     public static void main(String[] args) {
-        if (args.length < 1) {
-            usage();
+        String[] temp = new String[]{"-f", "json"};
+        CommandLine line = readCommandLine(temp);
+//        CommandLine line = readCommandLine(args);
+
+//        for (String className: args){
+//            Class<?> c = ReflectGrade.load(className);
+//            if (c == null) {
+//                System.err.printf("error loading class %s", className);
+//            } else {
+//                grade(c);
+//            }
+//        }
+    }
+
+    private static CommandLine readCommandLine(String[] args) {
+        Options options = new Options();
+        options.addOption(Option.builder("f").longOpt("format")
+                .desc("specify output, one of \'json\' (default) or \'text\'")
+                .hasArg(true)
+                .argName("output-format")
+                .build());
+        options.addOption(Option.builder().longOpt("pretty-print")
+                .desc("pretty-print json output")
+                .build());
+        options.addOption(Option.builder().longOpt("no-output")
+                .desc("don't produce any output (if user overriding)")
+                .build());
+        options.addOption(Option.builder("o")
+                .desc("save output to another file (if not specified, prints to standard out)")
+                .hasArg(true)
+                .argName("destination")
+                .build());
+        options.addOption(Option.builder("h").longOpt("help").build());
+        options.addOption(Option.builder("v").longOpt("version").build());
+
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.setWidth(80);
+        formatter.printHelp("jgrade", options);
+
+        CommandLineParser parser = new DefaultParser();
+        try {
+            System.out.println("parsing...");
+            CommandLine line  = parser.parse(options, args, false);
+            line.getArgList();
+            for (Option s : line.getOptions()) {
+                s.getValue();
+                System.out.print(s.getValue() +" ");
+            }
+        } catch (ParseException e) {
+            System.err.println(e.getMessage());
         }
 
-        for (String className: args){
-            Class<?> c = ReflectGrade.load(className);
-            if (c == null) {
-                System.err.printf("error loading class %s", className);
-            } else {
-                grade(c);
-            }
-        }
+        return null;
     }
 }
