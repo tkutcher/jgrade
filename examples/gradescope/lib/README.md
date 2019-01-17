6450e8e28e449c78363498d82f23b3ab8ce13fd6
Tim Kutcher <tkutcher@outlook.com>
2019-01-16 19:12:55 -0500

    example stuff

--- examples/gradescope/lib/README.md
+++ examples/gradescope/lib/README.md
@@ -1,5 +1,7 @@
 Here you would include all jars that the execution depends on. For this example:
 
-- jgrade-1.0.jar
-- junit-4.12.jar
+- checkstyle-8.12.jar
 - hamcrest-core-1.3.jar
+- jgrade-1.0.jar
+- json-20180813.jar
+- junit-4.12.jar
---
+++ examples/gradescope/src/main/java/staff/hello/GradeHello.java
@@ -0,0 +1,22 @@
+package staff.hello;
+
+import edu.jhu.cs.jgrade.CheckstyleGrader;
+import edu.jhu.cs.jgrade.Grade;
+import edu.jhu.cs.jgrade.Grader;
+import edu.jhu.cs.jgrade.gradedtest.GradedTestResult;
+
+public class GradeHello {
+    @Grade
+    public void runUnitTests(Grader grader) {
+        grader.runJUnitGradedTests(HelloTest.class);
+    }
+
+    @Grade
+    public void runCheckstyle(Grader grader) {
+        CheckstyleGrader checker = new CheckstyleGrader(10.0, 1.0,
+                "lib/checkstyle-8.12-all.jar", "src/main/java/student/hello");
+        checker.setConfig("res/sun_checks.xml");
+        GradedTestResult result = checker.runForGradedTestResult();
+        grader.addGradedTestResult(result);
+    }
+}
